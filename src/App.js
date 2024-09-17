808a3d0c709de48af3e42a13c68b88911f474226
Amanpreet-2004 <kaurcsamanpreet@gmail.com>
2024-09-17 12:11:33 +0530

    increment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import {useState } from 'react' 
 function App() {
+  const [count, setCount] =useState(1);
+  const increment = () =>{
+    setCount(count + 1);
+  }
+  const decrement =() =>{
+    setCount(count - 1);
+  }
   return (
    <div>
-    
+    <h1>CounterApp: {count}</h1>
+    <button onClick={increment}>btn1</button>
+    <button onClick={decrement}>btn2</button>
    </div>
   );
 }
